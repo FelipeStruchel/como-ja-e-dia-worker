58bd6ff5d50546558dbabc9d2cfe9bc662a736f9
Felipe Grego Struchel <75041313+FelipeStruchel@users.noreply.github.com>
2025-12-11 12:19:34 -0300

    fix: redis url

--- queues.js
+++ queues.js
@@ -1,9 +1,14 @@
 import { Queue } from "bullmq";
 import { config } from "./config.js";
 
+const connection = {
+    host: config.redisHost,
+    port: config.redisPort,
+};
+
 export const incomingQueue = new Queue(config.incomingQueueName, {
-    connection: { url: config.redisUrl },
+    connection,
 });
 
 export const sendQueueName = config.sendQueueName;
-export const redisConnection = { url: config.redisUrl };
+export const redisConnection = connection;
